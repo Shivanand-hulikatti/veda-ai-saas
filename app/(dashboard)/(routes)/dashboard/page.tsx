c65b52723e87ad88ab35027146752bd44e75abcb
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2024-08-25 22:13:45 +0530

    dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,10 +2,17 @@ import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = ()=> {
-  return ( <>
-    <p className="text-gray-500 text-2xl">Hiii</p>
-    
-    </>
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+    </div>
   );
 }
 export default DashboardPage;
